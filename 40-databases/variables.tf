d3ce2d90919d9b77a72e05cbde1db6603cc20918
devora-a <aravinddharla6@gmail.com>
2026-08-05 01:51:22 +0200

    ansible-v3

---
+++ 40-databases/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+    default = "roboshop"
+
+}
+
+variable "environment" {
+    default = "dev"
+}    
+
+variable "zone_id" {
+    default = "Z04138223ALQPP4SRQZFJ"
+} 
+
+variable "domain_name" {
+    default = "arrud.online"
+}
