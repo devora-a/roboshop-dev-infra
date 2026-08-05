18eafc6ee517e09a38227744e70d199bcc89c418
devora-a <aravinddharla6@gmail.com>
2026-08-05 03:57:10 +0200

    terraform

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -7,3 +7,13 @@ resource "aws_route53_record" "mongodb" {
   records = [aws_instance.mongodb.private_ip]
   allow_overwrite = true
 }
+
+# redis-dev.arrud.online
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+}
