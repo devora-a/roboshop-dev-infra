316291ea68024bb64b80de10e844c4947aed125a
devora-a <aravinddharla6@gmail.com>
2026-08-05 03:28:52 +0200

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -22,8 +22,8 @@ resource "aws_security_group_rule" "mongodb_user" {
 # mongodb allowing connections from bastion port 22
 resource "aws_security_group_rule" "mongodb_bastion" {
   type              = "ingress"
-  from_port         = 27017
-  to_port           = 27017
+  from_port         = 22
+  to_port           = 22
   protocol          = "tcp"
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.mongodb_sg_id
