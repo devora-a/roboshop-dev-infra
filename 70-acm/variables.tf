01485c751fe70d22c2c9aedd172b70d2d722d5b5
devora-a <aravinddharla6@gmail.com>
2026-08-12 02:58:59 +0200

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "app_version" {
+    default = "v3"
+}
+
+variable "zone_id" {
+    default = "Z04138223ALQPP4SRQZFJ"
+}    
+
+variable "domain_name" {
+    default = "arrud.online"
+}
+ 
