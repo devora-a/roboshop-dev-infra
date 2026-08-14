7ca33b47dc0581da96331116cc17d9a8c5b50a7d
devora-a <aravinddharla6@gmail.com>
2026-08-14 02:58:03 +0200

    terraform

--- 70-acm/variables.tf
+++ 70-acm/variables.tf
@@ -10,9 +10,9 @@ variable "app_version" {
     default = "v3"
 }
 
-variable "zone_id" {
+ variable "zone_id" {
     default = "Z04138223ALQPP4SRQZFJ"
-}    
+}   
 
 variable "domain_name" {
     default = "arrud.online"
--- 80-frontend_alb/variables.tf
+++ 80-frontend_alb/variables.tf
@@ -6,15 +6,10 @@ variable "environment" {
     default = "dev"
 }
 
-variable "app_version" {
-    default = "v3"
-}
-
-/* variable "zone_id" {
+variable "zone_id" {
     default = "Z04138223ALQPP4SRQZFJ"
-}  */  
+}  
 
 variable "domain_name" {
-    default = "arrud.online"
+    default = "daws90s.shop"
 }
- 
