6e0dbc8ea3f45c6459365c78d887d50c720f1404
devora-a <aravinddharla6@gmail.com>
2026-08-20 16:20:10 +0200

    terraform cdn

---
+++ 95-cdn/variables.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+    default = "dev"
+}
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "zone_id" {
+    default = "Z04138223ALQPP4SRQZFJ"
+}  
+
+variable "domain_name" {
+    default = "arrud.online"
+}
